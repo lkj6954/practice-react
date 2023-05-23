af6314d4aca2271e4f807a1a17f4cee095ff72dd
lkj6954 <rltkwpdntm@naver.com>
2023-05-23 18:44:17 +0900

    add : Create Item component using bootstrap, map and imported data

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
 import './App.css';
+import importedData from './data.js';
 
 function App() {
+    let [data] = useState(importedData);
+    let [imgSrc, setImgSrc] = useState([
+        'https://codingapple1.github.io/shop/shoes1.jpg',
+        'https://codingapple1.github.io/shop/shoes2.jpg',
+        'https://codingapple1.github.io/shop/shoes3.jpg',
+    ]);
     return (
         <div className='App'>
             <Navbar bg='dark' variant='dark'>
@@ -15,7 +23,30 @@ function App() {
                     </Nav>
                 </Container>
             </Navbar>
+            <div className='main-bg'></div>
+            <Container>
+                <Row>
+                    {data.map((a, i) => {
+                        return (
+                            <Item
+                                dataDictionary={data[i]}
+                                imgSrcElement={imgSrc[i]}
+                            />
+                        );
+                    })}
+                </Row>
+            </Container>
         </div>
+    );
+}
+
+function Item(props) {
+    return (
+        <Col className='col'>
+            <img alt='shoes' src={props.imgSrcElement} width='80%' />
+            <h4>{props.dataDictionary.title}</h4>
+            <p>{props.dataDictionary.price}</p>
+        </Col>
     );
 }
 
