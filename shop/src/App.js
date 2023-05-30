605c41a4b3f333de5a782b9bfccbcad320947c3d
lkj6954 <rltkwpdntm@naver.com>
2023-05-30 18:04:05 +0900

    feat : GET data when clicking btn using axios

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,18 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import importedData from './data.js';
 import Detail from './pages/Detail.js';
-import { Link, Route, Routes, useNavigate, Outlet } from 'react-router-dom';
+import { Link, Route, Routes, Outlet } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
-    let [data] = useState(importedData);
-    let [imgSrc, setImgSrc] = useState([
+    let [data, setData] = useState(importedData);
+    let [imgSrc] = useState([
         'https://codingapple1.github.io/shop/shoes1.jpg',
         'https://codingapple1.github.io/shop/shoes2.jpg',
         'https://codingapple1.github.io/shop/shoes3.jpg',
+        'https://codingapple1.github.io/shop/shoes4.jpg',
+        'https://codingapple1.github.io/shop/shoes5.jpg',
+        'https://codingapple1.github.io/shop/shoes6.jpg',
+        'https://codingapple1.github.io/shop/shoes7.jpg',
+        'https://codingapple1.github.io/shop/shoes8.jpg',
+        'https://codingapple1.github.io/shop/shoes9.jpg',
     ]);
+    let [btnClickCnt, setBtnClickCnt] = useState(0);
+    let [isLoading, setIsLoading] = useState(false);
+
+    let fetchData = (url) => {
+        axios
+            .get(url)
+            .then((result) => {
+                setData((prevData) => [...prevData, ...result.data]);
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsLoading(false);
+                console.log('axios get 실패');
+            });
+    };
+
+    useEffect(() => {
+        if (btnClickCnt === 1) {
+            fetchData('https://codingapple1.github.io/shop/data2.json');
+        } else if (btnClickCnt === 2) {
+            fetchData('https://codingapple1.github.io/shop/data3.json');
+        }
+    }, [btnClickCnt]);
+
+    useEffect(() => {
+        console.log(isLoading);
+    }, [isLoading]);
 
     return (
         <div className='App'>
@@ -30,12 +64,29 @@ function App() {
                                 {data.map((a, i) => {
                                     return (
                                         <Item
+                                            key={i}
                                             dataDictionary={data[i]}
                                             imgSrcElement={imgSrc[i]}
                                         />
                                     );
                                 })}
                             </Row>
+                            {isLoading ? (
+                                <p>상품 불러오는 중...</p>
+                            ) : (
+                                btnClickCnt !== 2 && (
+                                    <button
+                                        onClick={() => {
+                                            setBtnClickCnt(
+                                                (prevCount) => prevCount + 1
+                                            );
+                                            setIsLoading(true);
+                                        }}
+                                    >
+                                        상품 더보기
+                                    </button>
+                                )
+                            )}
                         </Container>
                     }
                 />
